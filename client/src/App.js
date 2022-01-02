c84845b99d1d02f4e39a8c9cf44ab1f18cd2f1c8
Tejas <>
2022-01-02 20:19:24 +0530

    Modified logic for Routing  & Redirecting

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,51 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import "./App.css";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import AllPost from "./pages/AllPost/AllPost";
+import CreatePost from "./pages/CreatePost/CreatePost";
 import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import Profile from "./pages/Profile/Profile";
-import CreatePost from "./pages/CreatePost/CreatePost";
-import { createContext, useContext, useEffect, useReducer } from "react";
-import { initialState, reducer } from "./reducers/userReducer";
+import UserProfile from "./pages/UserProfile/UserProfile";
 import ViewPost from "./pages/ViewPost/ViewPost";
+import { initialState, reducer } from "./reducers/userReducer";
 
 export const UserContext = createContext(reducer);
 
 const AppRoutes = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
   const { dispatch } = useContext(UserContext);
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
-    console.log("user- ", user);
 
-    if (user) dispatch({ type: "USER", payload: user });
-    else navigate("/signin");
-  }, []);
+    if (user) {
+      dispatch({ type: "USER", payload: user });
+      if (pathname === "/signin" || pathname === "/signup") navigate("/");
+    } else {
+      if (pathname === "/create" || pathname === "/") navigate("/all");
+    }
+  }, [pathname]);
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route exact path="/profile" element={<Profile />} />
       <Route path="/create" element={<CreatePost />} />
       <Route path="/post/:id" element={<ViewPost />} />
+      <Route path="/profile/:userid" element={<UserProfile />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/all" element={<AllPost />} />
     </Routes>
   );
 };
