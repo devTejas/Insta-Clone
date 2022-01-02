e426a54d59c2c63bef147139b4b4eab6da9ea81a
Tejas <>
2022-01-02 20:25:10 +0530

    Made profile logic dynamic

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../App";
+import Loading from "../../components/Loading/Loading";
 import "./Profile.css";
 
 const Profile = () => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { state, dispatch } = useContext(UserContext);
 
   const fetchPosts = async () => {
@@ -21,29 +23,37 @@ const Profile = () => {
   useEffect(() => fetchPosts(), []);
 
   return (
-    <div className="profile">
-      <div className="profileContainer">
-        <div className="profileImage">
-          <img src="/sirsourav.jpg" alt="Person Image" />
-        </div>
-        <div className="profileDescription">
-          <h4>{state?.name}</h4>
-          <div className="">
-            <h6>40 Posts</h6>
-            <h6>40 Followers</h6>
-            <h6>40 Following</h6>
+    <>
+      {loading ? (
+        <div className="profile">
+          <div className="profileContainer">
+            <div className="profileImage">
+              <img src="/sirsourav.jpg" alt="Person Image" />
+            </div>
+            <div className="profileDescription">
+              <h4>{state?.name}</h4>
+              <div className="">
+                <h6>{posts.length} Posts</h6>
+                <h6>40 Followers</h6>
+                <h6>40 Following</h6>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="gallery">
-        {posts.map((post) => (
-          <div className="itemContainer" key={post._id}>
-            <img className="item" src={post.photo} alt="" />
+          <div className="gallery">
+            {posts?.map((post) => (
+              <div className="itemContainer" key={post._id}>
+                <img className="item" src={post.photo} alt="" />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      {!posts.length && <h2 className="noPostsText">NO POSTS TO DISPLAY!</h2>}
-    </div>
+          {!posts?.length && (
+            <h2 className="noPostsText">NO POSTS TO DISPLAY!</h2>
+          )}
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 };
 
