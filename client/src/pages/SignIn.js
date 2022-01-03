d39d1d3507e7de3a1ebd1e59dec1046ea5ad78f4
Tejas <>
2022-01-03 20:37:29 +0530

    Modified logic for signin

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -3,7 +3,6 @@ import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../App";
 import { signIn } from "../services/UserAuthentication";
-
 const SignIn = () => {
   const { state, dispatch } = useContext(UserContext);
 
@@ -13,13 +12,8 @@ const SignIn = () => {
 
   const signInUser = async (guestUser = false) => {
     try {
-      const { token, user, error } = await signIn(email, password, guestUser);
+      const { error } = await signIn(email, password, guestUser);
       if (error) throw error;
-
-      localStorage.setItem("jwt", token);
-      localStorage.setItem("user", JSON.stringify(user));
-      dispatch({ type: "USER", payload: user });
-
       M.toast({ html: "Signed in successfully!", classes: "green" });
       navigate("/");
     } catch (err) {
@@ -28,49 +22,51 @@ const SignIn = () => {
   };
 
   return (
-    <div className="mycard">
-      <div className="card auth-card">
-        <h2>Instagram</h2>
-        <div className="" style={{ textAlign: "left" }}>
-          <label className="label-field">
-            Email
-            <input
-              className="input-field"
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-          <label className="label-field">
-            Password
-            <input
-              className="input-field"
-              type="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
+    <div className="container signUpContainer">
+      <div className="mycard">
+        <div className="card auth-card">
+          <h2>Instagram</h2>
+          <div className="" style={{ textAlign: "left" }}>
+            <label className="label-field">
+              Email
+              <input
+                className="input-field"
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label className="label-field">
+              Password
+              <input
+                className="input-field"
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="loginButtonsContainer">
+            <button
+              onClick={() => signInUser(false)}
+              className="btn waves-effect waves-light #64b5f6 blue darken-1"
+            >
+              Login
+            </button>
+            <button
+              className="guestLoginBtn"
+              // className="btn waves-effect waves-light #E76F51 blue darken-1 guestLoginBtn"
+              onClick={() => signInUser(true)}
+            >
+              GUEST LOGIN
+            </button>
+          </div>
+          <Link className="linkText" to="/signup">
+            Don't have an account?
+          </Link>
         </div>
-        <div className="loginButtonsContainer">
-          <button
-            onClick={() => signInUser(false)}
-            className="btn waves-effect waves-light #64b5f6 blue darken-1"
-          >
-            Login
-          </button>
-          <button
-            className="guestLoginBtn"
-            // className="btn waves-effect waves-light #E76F51 blue darken-1 guestLoginBtn"
-            onClick={() => signInUser(true)}
-          >
-            GUEST LOGIN
-          </button>
-        </div>
-        <Link className="linkText" to="/signup">
-          Don't have an account?
-        </Link>
       </div>
     </div>
   );
