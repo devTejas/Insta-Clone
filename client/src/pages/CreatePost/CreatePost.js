eef6855ac238c85dc0101749c8c0d069c74e7ff6
Tejas <>
2022-01-03 20:38:49 +0530

    Wrote logic for showing image before uploading

--- client/src/pages/CreatePost/CreatePost.js
+++ client/src/pages/CreatePost/CreatePost.js
@@ -7,7 +7,31 @@ const CreatePost = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [images, setImages] = useState(null);
+  const [imageURL, setImageURL] = useState(null);
   const navigate = useNavigate();
+
+  const uploadImage = async () => {
+    const data = new FormData();
+    data.append("file", images);
+    data.append("upload_preset", "insta-clone");
+    data.append("cloud_name", "pleasecontrol");
+    const url = await fetch(
+      "https://api.cloudinary.com/v1_1/pleasecontrol/image/upload",
+      {
+        method: "post",
+        body: data,
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => data.secure_url)
+      .catch((err) => {
+        console.log(err);
+        throw err;
+      });
+
+    // console.log(url);
+    return url;
+  };
 
   const uploadPost = async () => {
     try {
@@ -39,76 +63,64 @@ const CreatePost = () => {
           }
         });
     } catch ({ error }) {
-      console.log(err);
+      console.log(error);
       M.toast({ html: error, classes: "#c62828 red darken-3" });
     }
   };
 
-  const uploadImage = async () => {
-    const data = new FormData();
-    data.append("file", images);
-    data.append("upload_preset", "insta-clone");
-    data.append("cloud_name", "pleasecontrol");
-    const url = await fetch(
-      "https://api.cloudinary.com/v1_1/pleasecontrol/image/upload",
-      {
-        method: "post",
-        body: data,
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => data.secure_url)
-      .catch((err) => {
-        console.log(err);
-        throw err;
-      });
-
-    console.log(url);
-    return url;
+  // render image before uploading
+  const renderImage = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (e) => setImageURL(e.target.result);
+    setImages(file);
   };
+
   return (
-    <div className="card input-field uploadFileCard">
-      <div className="" style={{ textAlign: "left" }}>
-        <label className="inputLabel">
-          Title
-          <input
-            type="text"
-            placeholder="Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </label>
-        <label className="inputLabel">
-          Body
-          <textarea
-            type="text"
-            placeholder="Body"
-            defaultValue={body}
-            onChange={(e) => setBody(e.target.value)}
-          ></textarea>
-        </label>
-        <img src="" alt="" />
-      </div>
-      <div className="file-field input-field">
-        <div className="btn #64b5f6 blue darken-1">
-          <span>Upload Image</span>
-          <input
-            type="file"
-            accept=".png, .jpeg, .jpg, .gif"
-            // multiple={true}
-            onChange={(e) => setImages(e.target.files[0])}
-          />
+    <div className="container">
+      <div className="card input-field uploadFileCard">
+        <div className="" style={{ textAlign: "left" }}>
+          <label className="inputLabel">
+            Title
+            <input
+              type="text"
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </label>
+          <label className="inputLabel">
+            Body
+            <textarea
+              type="text"
+              placeholder="Body"
+              defaultValue={body}
+              onChange={(e) => setBody(e.target.value)}
+            ></textarea>
+          </label>
         </div>
-        <div className="file-path-wrapper">
-          <input type="text" className="file-path validate" />
+        <div className="file-field input-field">
+          <div className="btn #64b5f6 blue darken-1">
+            <span>Upload Image</span>
+            <input
+              type="file"
+              accept=".png, .jpeg, .jpg, .gif"
+              // multiple={true}
+              onChange={(e) => renderImage(e.target.files[0])}
+            />
+          </div>
+          <div className="file-path-wrapper">
+            <input type="text" className="file-path validate" />
+          </div>
         </div>
+        <button
+          className="btn waves-effect waves-light #64b5f6 blue darken-1"
+          onClick={uploadPost}
+        >
+          Submit Post
+        </button>
       </div>
-      <button
-        className="btn waves-effect waves-light #64b5f6 blue darken-1"
-        onClick={uploadPost}
-      >
-        Submit Post
-      </button>
+      <img src={imageURL} alt="" className="uploadedImage" />
     </div>
   );
 };
