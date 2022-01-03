4665e4a39c1e32a152b342169a3636761e2dcad3
Tejas <>
2022-01-03 20:42:40 +0530

    Formatted code

--- server/models/post.js
+++ server/models/post.js
@@ -2,30 +2,13 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema.Types;
 
 const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  body: {
-    type: String,
-    required: true,
-  },
-  // photoURL
-  photo: {
-    type: String,
-    required: true,
-  },
-  // image: {
-  //   data: "Buffer",
-  //   contentType: "String",
-  //   default: [],
-  // },
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  photo: { type: String, required: true }, // photoURL
+  // image: { data: "Buffer", contentType: "String", default: [] },
   likes: [{ type: ObjectId, ref: "User" }],
   comments: [{ text: String, postedBy: { type: ObjectId, ref: "User" } }],
-  postedBy: {
-    type: ObjectId,
-    ref: "User",
-  },
+  postedBy: { type: ObjectId, ref: "User" },
 });
 
 mongoose.model("Post", postSchema);
