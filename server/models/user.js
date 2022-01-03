ec087ecc598aa1337b6b04a9a588c8c93d0abb7b
Tejas <>
2022-01-03 20:42:52 +0530

    Code Formatted

--- server/models/user.js
+++ server/models/user.js
@@ -2,17 +2,13 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema.Types;
 
 const userSchema = new mongoose.Schema({
-  name: {
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  imgURL: {
     type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
+    default:
+      "https://res.cloudinary.com/pleasecontrol/image/upload/v1641190938/personImage_fzcba7.jpg",
   },
   followers: [{ type: ObjectId, ref: "User" }],
   following: [{ type: ObjectId, ref: "User" }],
