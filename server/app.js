a589671e2cd8f2bc33e85c3248ea2bc2d93b310e
Tejas <55909123+devTejas@users.noreply.github.com>
2023-04-27 03:36:32 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
-const { MONGOURI } = require("./config/keys");
+// const { MONGOURI } = require("./config/keys");
 
-mongoose.connect(MONGOURI, {
+mongoose.connect("https://cloud.mongodb.com/v2/619a1d08b20c407fd4476838#clusters", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
