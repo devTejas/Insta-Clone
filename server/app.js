a16bd5cbba60c20131c744eb01af835494aee2ab
Tejas <55909123+devTejas@users.noreply.github.com>
2023-04-27 19:55:53 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
   const path = require("path");
   console.log(__dirname, path, PORT);
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve("/opt/render/project/src/", "client", "build", "index.html"))
   );
 }
 app.listen(PORT, () =>
