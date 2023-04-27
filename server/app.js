18a4f4b8aabbaf1016eb9e5fbc9218909fd06c1f
Tejas <55909123+devTejas@users.noreply.github.com>
2023-04-27 19:21:45 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -29,6 +29,7 @@ app.use(require("./routes/user"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
+  console.log(__dirname, path, PORT);
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
