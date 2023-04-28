21a88e35458831facd17905daf4fd1507b8fb9e1
Tejas <55909123+devTejas@users.noreply.github.com>
2023-04-28 17:39:35 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -31,7 +31,8 @@ if (process.env.NODE_ENV === "production") {
   const path = require("path");
   console.log(__dirname, path, PORT);
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve("/opt/render/project/src/", "client", "build", "index.html"))
+    // res.sendFile(path.resolve("/opt/render/project/src/", "client", "build", "index.html"))
+       res.redirect("https://flipkartish.netlify.app/");
   );
 }
 app.listen(PORT, () =>
